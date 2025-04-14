28646a3d91e19b875b2762a2363fab491b5265f4
TajulMunandar <tajulmunandar701@gmail.com>
2025-04-14 08:59:45 +0700

    feat(): Init Template Framework

---
+++ resources/views/dashboard/partials/main.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modernize Free</title>
+    @include('dashboard.partials.head')
+</head>
+
+<body>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        @include('dashboard.partials.sidebar')
+        <!--  Sidebar End -->
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            <!--  Header Start -->
+            @include('dashboard.partials.navbar')
+            <!--  Header End -->
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    @include('dashboard.partials.script')
+</body>
+
+</html>
